b7253e905d4eb7506bd42dce9d660e1fc9874bc5
Debora Maxinas <md.dumuta@gmail.com>
2022-10-22 16:28:33 +0300

    tema3

--- src/Main.java
+++ src/Main.java
@@ -39,6 +39,32 @@ public class Main {
 //*/
 ////        float aaa = 15 / 2f;P
 
+        Calculator calc = new Calculator();
+        Saluta();
+
+        int s = calc.sum(2,3);
+        System.out.println(s);
+        float r = calc.rest(10,3);
+        System.out.println(r);
+        float o1 = calc.oper1(-5,8,6);
+        System.out.println(o1);
+        float o2 = calc.oper2(55,9,9);
+        System.out.println(o2);
+        float o3 = calc.oper3(20,-3,5,8);
+        System.out.println(o3);
+        float o4 = calc.oper4(5,15,3,2,8,3);
+        System.out.println(o4);
+        Javi();
+        Smile();
+        float m = calc.med(1,2,3);
+        System.out.println(m);
+        double d = calc.distanta(2);
+        System.out.println(d);
+        float t = (float) calc.temperatura(100);
+        System.out.println(t+" grade Celsius");
+        String v = calc.viteza(1000,0,6,0);
+        System.out.println(v);
+
         LogicalOperations logicOp = new LogicalOperations();
         int biggest = logicOp.checkBiggerNumber(2,3);
         String fast = "FastTrackIT";
